ce955583ccc2875b2e361e8c60e9311be3e54328
itzelJimenez <itzeljimenez.do@gmail.com>
2019-06-14 23:50:14 -0500

    add get post with http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  users: string[] = ['Luis', 'Clara', 'Roberto']
+  users: string[] = ['Luis', 'Clara', 'Roberto'];
   name: string = "Jon Carter";
   age: number = 28;
+  posts: any = [];
+
+  constructor(private dataService: DataService) {
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+  }
 
   sayHello() {
     alert('Hello')
@@ -27,4 +36,5 @@ export class AppComponent {
     newUser.value = ''
     return false;
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorld } from './firstcomponent/firstcomponent';
-import { SecondComponentComponent } from './second-component/second-component.component';
 import { UserComponent } from './user/user.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorld,
-    SecondComponentComponent,
     UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
