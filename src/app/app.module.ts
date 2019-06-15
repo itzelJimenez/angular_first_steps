9ecb57e3a0d59f4716b9e51615efc16d49167b98
itzelJimenez <itzeljimenez.do@gmail.com>
2019-06-15 00:16:37 -0500

    add routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route} from '@angular/router'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 
-import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Route[] = [
+  {
+    path: '', 
+    component: HomeComponent
+  },
+  {
+    path: 'about', 
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
